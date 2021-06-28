94649b7c0598c5db7b356618ffdcfc198642e109
James Mellor <mellor29j@gmail.com>
2021-06-28 19:03:35 +0200

    Fixed Admin link showing in main menu for non-logged-in-user

--- includes/main_menu.php
+++ includes/main_menu.php
@@ -5,11 +5,11 @@
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li><a href="photographers.php">Photographers</a></li>
-                    <li><a href="admin/index.php">Admin</a></li>
                     <?php if (!$session->is_signed_in()) { ?>
                         <li><a href="login.php">Login</a></li>
                         <li><a href="register.php">Register</a></li>
                     <?php } else { ?>
+                        <li><a href="admin/index.php">Admin</a></li>
                         <li><a href="liked.php">My Liked Photos</a></li>
                         <li><a href="logout.php">Logout <?php echo $session->user_username; ?></a></li>
                     <?php } ?>
