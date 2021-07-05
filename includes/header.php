424a0d6526bcf142ebc5d4309938beeed8fd7216
James Mellor <mellor29j@gmail.com>
2021-07-05 15:46:26 +0200

    Stripped unused files

--- includes/header.php
+++ includes/header.php
@@ -12,20 +12,22 @@ require_once("admin/includes/config.php");
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  		<title><?php echo PAGE_TITLE; ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title><?php echo PAGE_TITLE; ?></title>
+
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/fontAwesome.css">
-        <!-- Highway Template https://templatemo.com/tm-520-highway -->
         <link rel="stylesheet" href="css/templatemo-style.css">
-        <link rel="stylesheet" href="css/useradded.css">
 
         <link href="https://fonts.googleapis.com/css?family=Kanit:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
-        <?php require_once("header_scripts.php"); ?>
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
     </head>
 
     <body>
--- includes/header_scripts.php
+++
@@ -1,5 +0,0 @@
-<script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-<script src="js/vendor/bootstrap.min.js"></script>
-<script src="js/main.js"></script>
