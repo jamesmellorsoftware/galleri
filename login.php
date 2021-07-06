f51e63295f47dee26cc7d01c6010a8c88ed03fa0
James Mellor <mellor29j@gmail.com>
2021-07-06 12:49:19 +0200

    Added sticky footer to registration and login pages

--- login.php
+++ login.php
@@ -24,43 +24,43 @@ if (isset($_POST['login'])) {
 }
 
 ?>
-
-<div class="noHeaderVideo grid-portfolio">
-    <div class="container text-center">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <?php if ($session->is_signed_in()) { ?>
-                <h4 class="bg-success">
-                    <?php echo LOGIN_SUCCESS_1; ?>
-                    <?php echo LOGIN_SUCCESS_2; if (isset($user->user_username)) echo $user->user_username; ?>.<br />
-                    <?php echo LOGIN_SUCCESS_3; ?>
-                </h4>
-            <?php } else { ?>
-                <?php foreach ($login_errors as $login_error) { ?>
-                    <h4 class="text-danger"><?php echo $login_error; ?></h4>
+<div class="wrapper">
+    <div class="noHeaderVideo grid-portfolio">
+        <div class="container text-center">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <?php if ($session->is_signed_in()) { ?>
+                    <h4 class="text-success">
+                        <?php echo LOGIN_SUCCESS_1; ?>
+                        <?php echo LOGIN_SUCCESS_2; if (isset($user->user_username)) echo $user->user_username; ?>.<br />
+                        <?php echo LOGIN_SUCCESS_3; ?>
+                    </h4>
+                <?php } else { ?>
+                    <?php foreach ($login_errors as $login_error) { ?>
+                        <h4 class="text-danger"><?php echo $login_error; ?></h4>
+                    <?php } ?>
+                    <form method="post" action="login.php">
+                        <div class="form-group">
+                            <input type="text"
+                            class="form-control <?php if (!empty($login_errors['username'])) echo "is-invalid "; ?>"
+                            name="user_username" placeholder="<?php echo LOGIN_PLACEHOLDER_USERNAME . "*"; ?>"
+                            value="<?php if (isset($user->user_username)) echo $user->user_username; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="password"
+                            class="form-control <?php if (!empty($login_errors['username'])) echo "is-invalid "; ?>"
+                            name="user_password" placeholder="<?php echo LOGIN_PLACEHOLDER_PASSWORD . "*"; ?>"
+                            value="<?php if (isset($user->user_password)) echo $user->user_password; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="login" class="btn btn-primary" value="<?php echo LOGIN_SUBMIT_BUTTON; ?>">
+                        </div>
+                    </form>
+                    <p><a href="register.php"><?php echo LOGIN_ALREADY_HAVE_ACCOUNT; ?></a></p>
                 <?php } ?>
-                <form method="post" action="login.php">
-                    <div class="form-group">
-                        <input type="text"
-                        class="form-control <?php if (!empty($login_errors['username'])) echo "is-invalid "; ?>"
-                        name="user_username" placeholder="<?php echo LOGIN_PLACEHOLDER_USERNAME . "*"; ?>"
-                        value="<?php if (isset($user->user_username)) echo $user->user_username; ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="password"
-                        class="form-control <?php if (!empty($login_errors['username'])) echo "is-invalid "; ?>"
-                        name="user_password" placeholder="<?php echo LOGIN_PLACEHOLDER_PASSWORD . "*"; ?>"
-                        value="<?php if (isset($user->user_password)) echo $user->user_password; ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" name="login" class="btn btn-primary" value="<?php echo LOGIN_SUBMIT_BUTTON; ?>">
-                    </div>
-                </form>
-                <p><a href="register.php"><?php echo LOGIN_ALREADY_HAVE_ACCOUNT; ?></a></p>
-            <?php } ?>
+            </div>
+            <div class="col-md-4"></div>
         </div>
-        <div class="col-md-4"></div>
     </div>
 </div>
-
 <?php require_once("includes/footer.php"); ?>
--- register.php
+++ register.php
@@ -34,66 +34,66 @@ if (isset($_POST['register'])) {
 }
 
 ?>
-
-<div class="noHeaderVideo grid-portfolio">
-    <div class="container text-center">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <?php if ($registration_successful) { ?>
-                <h4 class="bg-success"><?php echo REGISTRATION_SUCCESS_MESSAGE; ?></h4>
-                <a href="index.php"><?php echo REGISTRATION_BACK_TO_INDEX; ?></a>
-            <?php } else { ?>
-                <?php foreach ($registration_errors as $registration_error) { ?>
-                    <h4 class="text-danger"><?php echo $registration_error; ?></h4>
+<div class="wrapper">
+    <div class="noHeaderVideo grid-portfolio">
+        <div class="container text-center">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <?php if ($registration_successful) { ?>
+                    <h4 class="text-success"><?php echo REGISTRATION_SUCCESS_MESSAGE; ?></h4>
+                    <a href="index.php"><?php echo REGISTRATION_BACK_TO_INDEX; ?></a>
+                <?php } else { ?>
+                    <?php foreach ($registration_errors as $registration_error) { ?>
+                        <h4 class="text-danger"><?php echo $registration_error; ?></h4>
+                    <?php } ?>
+                    <form method="post" action="register.php"  enctype="multipart/form-data">
+                        <div class="form-group">
+                            <input type="text"
+                            class="form-control <?php if (isset($registration_errors['username'])) echo "is-invalid"; ?>"
+                            name="user_username" placeholder="<?php echo REGISTRATION_PLACEHOLDER_USERNAME . "*"; ?>"
+                            value="<?php if (isset($new_user->user_username)) echo htmlentities($new_user->user_username); ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="password"
+                            class="form-control <?php if (isset($registration_errors['password'])) echo "is-invalid"; ?>"
+                            name="user_password" placeholder="<?php echo REGISTRATION_PLACEHOLDER_PASSWORD . "*"; ?>"
+                            value="<?php if (isset($new_user->user_password)) echo htmlentities($new_user->user_password); ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="text"
+                            class="form-control <?php if (isset($registration_errors['firstname'])) echo "is-invalid"; ?>"
+                            name="user_firstname" placeholder="<?php echo REGISTRATION_PLACEHOLDER_FIRSTNAME . "*"; ?>"
+                            value="<?php if (isset($new_user->user_firstname)) echo htmlentities($new_user->user_firstname); ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="text"
+                            class="form-control <?php if (isset($registration_errors['lastname'])) echo "is-invalid"; ?>"
+                            name="user_lastname" placeholder="<?php echo REGISTRATION_PLACEHOLDER_LASTNAME . "*"; ?>"
+                            value="<?php if (isset($new_user->user_lastname)) echo htmlentities($new_user->user_lastname); ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="email"
+                            class="form-control <?php if (isset($registration_errors['email'])) echo "is-invalid"; ?>"
+                            name="user_email" placeholder="<?php echo REGISTRATION_PLACEHOLDER_EMAIL . "*"; ?>"
+                            value="<?php if (isset($new_user->user_email)) echo htmlentities($new_user->user_email); ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="user_image"
+                            class="<?php if (isset($registration_errors['file_upload'])) echo "text-danger"; ?>">
+                                <?php echo REGISTRATION_LABEL_USERIMAGE . "*"; ?>
+                            </label>
+                            <input type="file" name="user_image"
+                            class="form-control <?php if (isset($registration_errors['file_upload'])) echo "is-invalid"; ?>">
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="register" class="btn btn-primary" value="<?php echo REGISTRATION_REGISTER; ?>">
+                        </div>
+                    </form>
+                    <p><a href="login.php"><?php echo REGISTRATION_ALREADY_HAVE_ACCOUNT; ?></a></p>
                 <?php } ?>
-                <form method="post" action="register.php"  enctype="multipart/form-data">
-                    <div class="form-group">
-                        <input type="text"
-                        class="form-control <?php if (isset($registration_errors['username'])) echo "is-invalid"; ?>"
-                        name="user_username" placeholder="<?php echo REGISTRATION_PLACEHOLDER_USERNAME . "*"; ?>"
-                        value="<?php if (isset($new_user->user_username)) echo htmlentities($new_user->user_username); ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="password"
-                        class="form-control <?php if (isset($registration_errors['password'])) echo "is-invalid"; ?>"
-                        name="user_password" placeholder="<?php echo REGISTRATION_PLACEHOLDER_PASSWORD . "*"; ?>"
-                        value="<?php if (isset($new_user->user_password)) echo htmlentities($new_user->user_password); ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="text"
-                        class="form-control <?php if (isset($registration_errors['firstname'])) echo "is-invalid"; ?>"
-                        name="user_firstname" placeholder="<?php echo REGISTRATION_PLACEHOLDER_FIRSTNAME . "*"; ?>"
-                        value="<?php if (isset($new_user->user_firstname)) echo htmlentities($new_user->user_firstname); ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="text"
-                        class="form-control <?php if (isset($registration_errors['lastname'])) echo "is-invalid"; ?>"
-                        name="user_lastname" placeholder="<?php echo REGISTRATION_PLACEHOLDER_LASTNAME . "*"; ?>"
-                        value="<?php if (isset($new_user->user_lastname)) echo htmlentities($new_user->user_lastname); ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="email"
-                        class="form-control <?php if (isset($registration_errors['email'])) echo "is-invalid"; ?>"
-                        name="user_email" placeholder="<?php echo REGISTRATION_PLACEHOLDER_EMAIL . "*"; ?>"
-                        value="<?php if (isset($new_user->user_email)) echo htmlentities($new_user->user_email); ?>">
-                    </div>
-                    <div class="form-group">
-                        <label for="user_image"
-                        class="<?php if (isset($registration_errors['file_upload'])) echo "text-danger"; ?>">
-                            <?php echo REGISTRATION_LABEL_USERIMAGE . "*"; ?>
-                        </label>
-                        <input type="file" name="user_image"
-                        class="form-control <?php if (isset($registration_errors['file_upload'])) echo "is-invalid"; ?>">
-                    </div>
-                    <div class="form-group">
-                        <input type="submit" name="register" class="btn btn-primary" value="<?php echo REGISTRATION_REGISTER; ?>">
-                    </div>
-                </form>
-                <p><a href="login.php"><?php echo REGISTRATION_ALREADY_HAVE_ACCOUNT; ?></a></p>
-            <?php } ?>
+            </div>
+            <div class="col-md-4"></div>
         </div>
-        <div class="col-md-4"></div>
     </div>
 </div>
-
 <?php require_once("includes/footer.php"); ?>
